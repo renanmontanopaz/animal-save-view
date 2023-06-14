dafd132b14aee624dd45844bce701b20833842be
Renan Montano Paz <renanmontanopaz@outlook.com>
2023-06-13 21:15:57 -0300

    fazendo login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import Login from "@/views/Login.vue";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
