ab98ce08909fc5dfec5d6bc0a758c9dd12eb907c
Cesar <francacesar26@gmail.com>
2023-06-15 10:25:25 -0300

    Adicionada a rota para a pagina de cadastro dos animais no router e criado o protótipo inicial da pagina.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,27 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
 import Login from "@/views/Login.vue";
+import RegisterAnimal from "@/views/Protetora/RegisterAnimals.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
-]
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register-animal",
+    name: "RegisterAnimal",
+    component: RegisterAnimal,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Protetora/RegisterAnimals.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1>Register</h1>
+    <form @submit.prevent="onSubmit">
+      <div class="field">
+        <label class="label">Name</label>
+        <select v-model="animal.selectedNames" multiple>
+          <option v-for="name in animal.names" :key="name" :value="name">
+            {{ name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="field">
+        <label class="label">Teste</label>
+        <input class="input" type="email" v-model="animal.email">
+      </div>
+
+      <div class="field">
+        <label class="label">Password</label>
+        <input class="input" type="password" v-model="animal.password">
+      </div>
+
+      <button class="button is-primary" type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+interface Animal {
+  names: string[];
+  selectedNames: string[];
+  email: string;
+  password: string;
+}
+
+@Component
+export default class Register extends Vue {
+  public animal: Animal = {
+    names: ['Name1', 'Name2', 'Name3'], 
+    selectedNames: [], 
+    email: '',
+    password: '',
+  }
+
+  public onSubmit() {
+    // Here, you should send the animal data to your server.
+    console.log(this.animal);
+  }
+}
+</script>
