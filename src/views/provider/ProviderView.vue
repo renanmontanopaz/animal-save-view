a9c545243b026689796ab4534101a3943571b9d7
Allyson <jandreyallyson@gmail.com>
2023-06-19 18:54:37 -0300

    feat - tela de listagem de serviços estática

--- src/views/provider/ProviderView.vue
+++ src/views/provider/ProviderView.vue
@@ -1,7 +1,28 @@
 <template>
     <main>
         <section>
+            <div class="collumns is-fullwidth">
+                <h1 class="title">Lista de Serviços</h1>
 
+                <table class="table is-bordered">
+                    <thead>
+                        <tr>
+                            <th>Serviço</th>
+                            <th>Opções</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="serviceField">Serviço</th>
+
+                            <th class="buttonsField">
+                                <button class="button is-warning is-focused">Editar</button>
+                                <button class="button is-danger is-focused">Apagar</button>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </section>
     </main>
 </template>
