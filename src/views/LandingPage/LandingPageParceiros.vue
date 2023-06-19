a3db173291749be28872721e5b561328ac193793
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-19 16:43:40 -0300

    Configurando a tela Parceiros

---
+++ src/views/LandingPage/LandingPageParceiros.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="containerParceiros">
+  </section>
+</template>
+
+<style lang="scss">
+@import "~bulma/bulma.sass";
+@import url("https://fonts.googleapis.com/css2?family=Concert+One&family=Poppins:ital,wght@0,200;0,400;0,600;1,100&display=swap");
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class LandingPageParceiros extends Vue {
+
+}
+</script>
