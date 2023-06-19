beb8086a72c9f6599bd6ff7401905954284723bd
Allyson <jandreyallyson@gmail.com>
2023-06-19 18:10:19 -0300

    feat - criação campo de input custo do produto

--- src/views/provider/RegisterServiceView.vue
+++ src/views/provider/RegisterServiceView.vue
@@ -7,9 +7,19 @@
             <div class="field">
                 <label class="label">Nome do serviço</label>
                 <p class="control has-icons-left">
-                    <input class="input" placeholder="Ex: Ração">
+                    <input class="input" type="input" placeholder="Ex: Ração">
                     <span class="icon is-small is-left">
                         <i class="fa fa-bag-shopping"></i>
+                    </span>
+                </p>
+            </div>
+
+            <div class="field">
+                <label class="label">Custo</label>
+                <p class="control has-icons-left">
+                    <input class="input" type="number" placeholder="Ex: 100.00">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-money-bill"></i>
                     </span>
                 </p>
             </div>
