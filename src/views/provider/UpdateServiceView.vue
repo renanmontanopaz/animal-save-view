f54420b6cb1353c72c8277d1584b6c938961323a
Allyson <jandreyallyson@gmail.com>
2023-06-19 19:13:20 -0300

    feat - criação campo de descrição estático

--- src/views/provider/UpdateServiceView.vue
+++ src/views/provider/UpdateServiceView.vue
@@ -23,6 +23,16 @@
                     </span>
                 </p>
             </div>
+
+            <div class="field">
+                <label class="label">Descrição</label>
+                <p class="control has-icons-left">
+                    <textarea class="input" placeholder="Descrição"></textarea>
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-list"></i>
+                    </span>
+                </p>
+            </div>
         </main>
     </section>
 </template>
