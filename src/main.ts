dd7f37dc8846c66cbc1777930470ecc748e9d14b
Cesar <francacesar26@gmail.com>
2023-06-18 23:39:30 -0300

    Código comentado apagado

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@fortawesome/fontawesome-free/js/all'
-import 'bulma/bulma.sass'
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@fortawesome/fontawesome-free/js/all";
+import "bulma/bulma.sass";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
-/*export default {
-  setup() {
-    const router = useRouter();
-    const loginInstance = new Login();
-
-    // Verifica a autenticação antes de cada navegação
-
-  },
-};*/
+  render: (h) => h(App),
+}).$mount("#app");
