d1c12bf110f3a8cb0fc6b4d4aceb0f920a3ec067
Allyson <jandreyallyson@gmail.com>
2023-06-19 19:10:54 -0300

    feat - campo de input nome do serviço estático

--- src/views/provider/UpdateServiceView.vue
+++ src/views/provider/UpdateServiceView.vue
@@ -1,4 +1,21 @@
-<template></template>
+<template>
+    <section>
+        <main>
+            <div class="control">
+                <h1 class="title">Editar Serviço</h1>
+            </div>
+            <div class="field">
+                <label class="label">Nome do serviço</label>
+                <p class="control has-icons-left">
+                    <input class="input" type="input" placeholder="Ex: Ração">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-bag-shopping"></i>
+                    </span>
+                </p>
+            </div>
+        </main>
+    </section>
+</template>
 
 <script lang="ts"></script>
 
