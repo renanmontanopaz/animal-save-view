1c8b0a81ea99cdfc4d22cbf1259b218ffe8241d6
Allyson <jandreyallyson@gmail.com>
2023-06-19 19:00:45 -0300

    style - reestruturação da tabela de serviços

--- src/views/provider/ProviderView.vue
+++ src/views/provider/ProviderView.vue
@@ -1,33 +1,73 @@
 <template>
-    <main>
-        <section>
-            <div class="collumns is-fullwidth">
-                <h1 class="title">Lista de Serviços</h1>
+    <div class="columns is-fullwidth">
+        <h1 class="title">Lista de Serviços</h1>
 
-                <table class="table is-bordered">
-                    <thead>
-                        <tr>
-                            <th>Serviço</th>
-                            <th>Opções</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th class="serviceField">Serviço</th>
+        <table class="table is-bordered">
+            <thead>
+                <tr>
+                    <th>Serviço</th>
+                    <th>Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class="serviceField"> servico</th>
 
-                            <th class="buttonsField">
-                                <button class="button is-warning is-focused">Editar</button>
-                                <button class="button is-danger is-focused">Apagar</button>
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </section>
-    </main>
+                    <th>
+                        <button class="button is-warning is-focused">Editar</button>
+                        <button class="button is-danger is-focused">Apagar</button>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { TaskClient } from '@/client/Task.client';
+import { Task } from '@/model/Task';
+import router from '@/router';
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ProviderView extends Vue {
+
+    private taskClient: TaskClient = new TaskClient
+    public taskList: Task[] = []
+    public task: Task = new Task();
+
+    public mounted(): void {
+        this.listarTasks()
+        this.onClickDelete()
+    }
+
+    private listarTasks(): void {
+        this.taskClient.listAll().then(
+            success => {
+                console.log('Listagem de serviços concluída');
+            },
+            error => console.log(error)
+        )
+    }
+
+    public onClickDelete(): void {
+        this.taskClient.disable(this.task.id).then(
+            success => {
+                console.log('Serviço deletado com sucesso!')
+                this.task = new Task()
+            },
+            error => {
+                console.log(error)
+            }
+        )
+    }
+
+    public onClickEdit(id: number) {
+        router.push({ path: `/update/${id}` })
+    }
+
+}
+</script>
 
 <style lang="scss" scoped>
 .columns {
