b75eb9cc576179ef59a79b689b67a8d06c57929a
Allyson <jandreyallyson@gmail.com>
2023-06-19 18:05:40 -0300

    feat - campo de input nome do serviço

--- src/views/provider/RegisterServiceView.vue
+++ src/views/provider/RegisterServiceView.vue
@@ -1,4 +1,21 @@
-<template></template>
+<template>
+    <main>
+        <section>
+            <div class="control">
+                <h1 class="title">Cadastro de Produto</h1>
+            </div>
+            <div class="field">
+                <label class="label">Nome do serviço</label>
+                <p class="control has-icons-left">
+                    <input class="input" placeholder="Ex: Ração">
+                    <span class="icon is-small is-left">
+                        <i class="fa fa-bag-shopping"></i>
+                    </span>
+                </p>
+            </div>
+        </section>
+    </main>
+</template>
 
 <script lang="ts">
 </script>
