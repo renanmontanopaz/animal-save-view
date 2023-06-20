17bfec2370589b62d73ce21f0751ebefdc65d5b2
Renan Montano Paz <renanmontanopaz@outlook.com>
2023-06-20 18:29:38 -0300

    fazendo manager users

--- src/views/Administrator/ManagerUsers.vue
+++ src/views/Administrator/ManagerUsers.vue
@@ -11,27 +11,37 @@ import {Component} from "vue-property-decorator";
 import {Message} from "@/model/Message";
 import {AdminClient} from "@/client/Admin.client";
 import {User} from "@/model/User";
+import {AssociateClient} from "@/client/Associate.client";
+import {CaregiverClient} from "@/client/Caregiver.client";
+import {ProviderClient} from "@/client/Provider.client";
 
 @Component
 export default class ManagerUsers extends Vue {
   public notificacao: Message = new Message();
   isVisible = false;
   private adminClient: AdminClient = new AdminClient();
-  public userList: User[] = []
-
+  public userList: Object[] = []
+  public associateClient: AssociateClient = new AssociateClient()
+  public caregiverClient: CaregiverClient = new CaregiverClient()
+  public providerClient: ProviderClient = new ProviderClient()
   public mounted(): void {
     this.ListUsers();
   }
   public ListUsers(): void {
-    this.adminClient.listUsers().then(
-        success => {
-          this.userList = success
-          console.log(this.userList)
+    Promise.all([
+      this.associateClient.listAll(),
+      this.caregiverClient.listAll(),
+      this.providerClient.listAll()
+    ]).then(
+        ([associateUsers, caregiverUsers, providerUsers]) => {
+          this.userList = [...associateUsers, ...caregiverUsers, ...providerUsers];
+          console.log(this.userList);
         },
         error => {
           console.log(error)
         }
     )
+
   }
 
 }
