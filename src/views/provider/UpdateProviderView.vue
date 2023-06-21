65928f1ab8d3346908c6d029b89613dc4cea89ef
Allyson <jandreyallyson@gmail.com>
2023-06-21 16:55:55 -0300

    feat - input nome fantasia estático

--- src/views/provider/UpdateProviderView.vue
+++ src/views/provider/UpdateProviderView.vue
@@ -1,4 +1,20 @@
-<template></template>
+<template>
+    <main>
+        <section>
+            <div class="control">
+                <h1 class="title">Editar perfil</h1>
+            </div>
+            <div class="align_inputs">
+                <div class="field">
+                    <label class="label">Nome fantasia</label>
+                    <div class="control">
+                        <input class="input" type="text" placeholder="Nome fantasia">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
 
 <script lang="ts"></script>
 
