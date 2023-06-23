5f3bed9051eb2b3f40a2f6332488f1ec50ddf6c6
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-23 18:22:00 -0300

    estilizando landing page responsiva partners

--- src/views/LandingPage/Partners.vue
+++ src/views/LandingPage/Partners.vue
@@ -106,6 +106,46 @@
   }
 }
 
+@media (min-height:810px) and (max-height:950px) {
+
+  .containerParceiros {
+    width: 100%;
+    height: 100vh;
+    background-color: #EBE3CC;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 4.6em;
+
+    .containerDivs {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+      .ImgCachorro {
+        display: none;
+      }
+
+      .containerImageParceiros {
+        width: 17em;
+        height: 10em;
+        background-color: #FBBD08;
+        border-radius: 1em;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        .containerImg {
+          width: 90%;
+        }
+      }
+    }
+  }
+}
 </style>
 
 <script lang="ts">
