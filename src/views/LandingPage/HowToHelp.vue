7a002b2e48b595d5048413e3493523040844e758
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-24 17:29:32 -0300

    estilizando landing page Como Ajudar

---
+++ src/views/LandingPage/HowToHelp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="modal">
+    <div class="modal-background">
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <h1 class="modal-card-title">Como Ajudar</h1>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <h3>Como você pode ajudar os animais através da animal save?</h3>
+          <p>Não custa quase nada ajudar! E é bem facil!</p>
+          <ol>
+            <li>Você faz o cadastro do seu nome e CPF/CNPJ</li>
+            <li>Você escolhe um valor para ajudar, que será debitado da sua conta mensalmente.</li>
+            <li>Você cadastra a sua conta corrente para o débito uma única vez, e pronto!</li>
+          </ol>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<style lang="scss" scoped></style>
