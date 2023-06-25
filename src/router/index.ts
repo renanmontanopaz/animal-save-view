94b9cd7144b470eb0c8acba523a9c61959b60969
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-25 18:36:01 -0300

    tela register occurences

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "@/views/LandingPage/Home.vue"
-import AboutUs from '@/views/LandingPage/AboutUs.vue';
-import Partners from '@/views/LandingPage/Partners.vue';
-import Footer from '@/views/LandingPage/Footer.vue';
 import Header from '@/views/LandingPage/Header.vue';
+import RegisterOccurences from '@/views/LandingPage/RegisterOccurences.vue';
 import Login from "@/views/Login.vue";
 import RegisterAnimal from "@/views/Protetora/RegisterAnimals.vue";
 import Register from "@/views/RegisterUsers/Register.vue";
@@ -12,7 +9,6 @@ import Associate from "@/views/associate/Associate.vue";
 import Administrator from "@/views/Administrator/Administrator.vue";
 import { Token } from "@/model/Token";
 import Modal from "@/views/Modal.vue";
-import HeaderVue from '@/views/LandingPage/Header.vue';
 
 Vue.use(VueRouter);
 const loginInstance = new Login();
@@ -40,6 +36,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Header",
     component: Header,
+  },
+  {
+    path: "/register-occurences",
+    name: "Register Occurences",
+    component: RegisterOccurences,
   },
   {
     path: "/associado",
---
+++ src/views/LandingPage/RegisterOccurences.vue
@@ -0,0 +1,70 @@
+<template>
+  <section>
+    <div class="form">
+      <div class="field">
+        <label class="label">Nome</label>
+        <div class="control">
+          <input class="input" type="text" v-model="occurences.name" placeholder="Text input">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Contato</label>
+        <div class="control">
+          <input class="input" type="text" v-model="occurences.contact" placeholder="Text input">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Descrição</label>
+        <div class="control">
+          <input class="input" type="text" v-model="occurences.description" placeholder="Text input">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Ponto de referencia</label>
+        <div class="control">
+          <input class="input" type="text" v-model="occurences.referenceLocal" placeholder="Text input">
+        </div>
+      </div>
+      <div class="field">
+        <div class="select">
+          <select v-model="occurences.situation">
+            <option>Emergencia</option>
+            <option>Urgencia</option>
+          </select>
+        </div>
+      </div>
+      <div class="control">
+        <button @click="RegisterOccurences()" class="button is-success is-focused">Cadastrar</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+
+import { OcurrencesClient } from '@/client/Ocurrences.client';
+import { Occurrences } from '@/model/Occurrences';
+import { Component, Vue } from 'vue-property-decorator'
+
+
+@Component
+export default class RegisterOccurences extends Vue {
+
+  public ocurrencesClient: OcurrencesClient = new OcurrencesClient();
+  public occurences: Occurrences = new Occurrences;
+
+  public RegisterOccurences(): void {
+    this.ocurrencesClient.save(this.occurences).then(
+      success => {
+        console.log(success)
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+}
+
+</script>
+
+<style lang="scss" scoped></style>
