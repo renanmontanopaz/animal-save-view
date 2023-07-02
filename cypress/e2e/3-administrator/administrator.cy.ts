3da10de3f4ee6433bedb34363c06d2dcb967c8b3
Renan Montano Paz <renanmontanopaz@outlook.com>
2023-07-02 16:42:46 -0300

    teste administrator parte 4

--- cypress/e2e/3-administrator/administrator.cy.ts
+++ cypress/e2e/3-administrator/administrator.cy.ts
@@ -28,20 +28,20 @@ describe('Administrator Page', () => {
         it('Deve estar na aba de "Usuários pendentes" ao carregar a página', () => {
             cy.get('.is-active > span').should('exist')
         })
-        it('Deve aparecer Modal com detalhes do usuário ao clicar em detalhar', () => {
+        /*it('Deve aparecer Modal com detalhes do usuário ao clicar em detalhar', () => {
             cy.get('#button-detalhar').click()
-        })
+        })*/
         /*it('Deve aprovar um usuários ao clicar no botão APROVAR',() => {
             cy.get('#button-aprovar').click()
             cy.wait(axios.HttpStatusCode.Ok)
         })*/
-        /*it('Deve aparecer o container "Nenhum usuário pendente!" caso não existe usuários pendentes', () =>{
+        it('Deve aparecer o container "Nenhum usuário pendente!" caso não existe usuários pendentes', () =>{
             cy.get('#notOccurrence').should('exist')
-        })*/
-        it('Deve sair do modal ao apertar no botão "Voltar"',() =>{
+        })
+        /*it('Deve sair do modal ao apertar no botão "Voltar"',() =>{
             cy.get('#button-detalhar').click()
             cy.get('#button-voltar').click()
-        })
+        })*/
         it('Deve ir a aba Usuários aprovados ao clicar',() =>{
             cy.get(':nth-child(2) > span').click()
         })
@@ -53,6 +53,42 @@ describe('Administrator Page', () => {
         })
         it('Deve ir a aba Gerenciar Usuários ao clicar',() =>{
             cy.get(':nth-child(5) > span').click()
+        })
+    })
+    context('Na aba Gerenciar usuários e na TAB Protetor(a)', () =>{
+
+        beforeEach(() => {
+            cy.viewport("macbook-15")
+            cy.visit('http://localhost:3000/login')
+            cy.get(':nth-child(1) > .control > .input').type('admin@admin.com')
+            cy.get(':nth-child(2) > .control > .input').type('admin')
+            cy.get('#button-login').click()
+            cy.wait(1000)
+            cy.get(':nth-child(5) > span').click()
+        })
+
+        it('deve expandir detalhes do usuário ao clicar em DETALHAR',() =>{
+            cy.get(':nth-child(1) > .card-header > .card-header-icon').click()
+            cy.get('.card-content').should('exist')
+        })
+        it('deve fechar detalhes do usuário ao clicar novamente em DETALHAR',() =>{
+            cy.get(':nth-child(1) > .card-header > .card-header-icon').click()
+            cy.get('.card-content').should('exist')
+            cy.wait(1000)
+            cy.get(':nth-child(1) > .card-header > .card-header-icon').click()
+            cy.get('.card-content').should('not.exist')
+        })
+        it('Deve abrir modal de editar usuário ao clicar em EDITAR',() => {
+            cy.get(':nth-child(1) > .card-footer > .card-footer-item').click()
+            cy.get('.modal-container').should('exist')
+        })
+        it('deve editar um dado do usuários, salvar e fechar a modal',() =>{
+            cy.get(':nth-child(1) > .card-footer > .card-footer-item').click()
+            cy.get('.modal-container').should('exist')
+            cy.get('[placeholder="Nome"]').type('editado')
+            cy.get('#button-aprovar').click()
+            cy.wait(2000)
+            cy.get('#button-voltar').click()
         })
     })
 })
